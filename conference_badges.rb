bc357ac3760c09121324db02af00cd1a3f84b318
Kristina Tong <kristina.tong@gmail.com>
2018-08-02 20:48:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,7 @@ end
 def assign_rooms(array)
   badge_array = []
   for i in (0...array.length)
-    room = i+1
-    badge_array.push("Hello, #{array[i]}! You'll be assigned to room #{room}!")
+    badge_array.push("Hello, #{array[i]}! You'll be assigned to room #{i+1}!")
   end
   badge_array
 end
