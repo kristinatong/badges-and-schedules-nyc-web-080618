35a3e49256c27e2bc7586b089fb486e7017345d1
Kristina Tong <kristina.tong@gmail.com>
2018-08-02 20:41:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,13 @@ def badge_maker(string)
 end
 
 def batch_badge_creator(array)
-  for i in (0..array.length)
+  badge_array = []
+  for i in (0...array.length)
+    badge_array.push("Hello, my name is #{array[i]}.")
+  end
+  badge_array
+end
+
+def assign_rooms(array)
+  
+end
